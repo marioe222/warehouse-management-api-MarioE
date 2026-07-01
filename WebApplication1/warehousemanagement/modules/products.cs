677a6df387a7264e3ca146d28a1f033128916d33
John Doe <john.doe@example.com>
2026-07-01 16:13:49 +0300

    create domain models

---
+++ WebApplication1/warehousemanagement/modules/products.cs
@@ -0,0 +1,17 @@
+﻿namespace warehousemanagement.modules;
+
+public class products
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Sku { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int QuantityInStock { get; set; }
+    public string SupplierName { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public Boolean IsArchived { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdatedAt { get; set; }
+    
+}
---
+++ WebApplication1/warehousemanagement/modules/productsImages.cs
@@ -0,0 +1,8 @@
+﻿namespace warehousemanagement.modules;
+
+public class productsImages
+{
+    public Guid ProductId { get; set; }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+}
